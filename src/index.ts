a5aab3c6e79b2efac48c846bfc01cf366874c18b
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-05-22 10:47:10 -0300

    testing get requests.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import fs from "fs"
+
+const PORT = 8080
+let counterItems = 0
+let counterItemRandom = 0
 
 const app = express()
 
-app.listen("3001", () => {
-  console.log("server running!")
+app.get("/items", async (req: Request, res: Response) => {
+  counterItems++
+  try {
+    const products = await fs.promises.readFile("./src/products.txt", "utf-8")
+    const productsParse = JSON.parse(products)
+    const response = {
+      items: productsParse,
+      cantidad: productsParse.length,
+    }
+    res.send(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get("/item-random", async (req: Request, res: Response) => {
+  counterItemRandom++
+  try {
+    const products = await fs.promises.readFile("./src/products.txt", "utf-8")
+    const productsParse = JSON.parse(products)
+    const randomNumber = Math.floor(Math.random() * productsParse.length)
+    const response = {
+      item: productsParse[randomNumber],
+    }
+    res.send(response)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get("/visitas", (req: Request, res: Response) => {
+  res.send({ visitas: { items: counterItems, item: counterItemRandom } })
+})
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}!`)
 })
