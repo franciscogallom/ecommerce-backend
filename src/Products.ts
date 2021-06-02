eeafa89eaee0288d4b63edc897d4f96fec0dbf22
franciscogallom <franciscogallomolinuevo@gmail.com>
2021-06-02 10:54:33 -0300

    put, delete, router and statics.

--- src/Products.ts
+++ src/Products.ts
@@ -34,4 +34,24 @@ export class Products {
     this.products.push(newProduct)
     return newProduct
   }
+
+  deleteProductById(idSearched: number) {
+    const indexOfIdSearched = this.products.findIndex(
+      (element) => element.id === idSearched
+    )
+    return indexOfIdSearched !== -1
+      ? this.products.splice(indexOfIdSearched, 1)
+      : false
+  }
+
+  updateProduct(product: ProductReceived, idSearched: number) {
+    const indexOfIdSearched = this.products.findIndex(
+      (element) => element.id === idSearched
+    )
+    if (indexOfIdSearched !== -1) {
+      this.products[indexOfIdSearched] = { ...product, id: idSearched }
+      return product
+    }
+    return false
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,19 @@
 import express, { Request, Response, NextFunction } from "express"
-import { Products } from "./Products"
-import { productsMock } from "./productsMock"
+import path from "path"
+import productos from "./productos"
 
 const PORT = 8080
-const products = new Products(productsMock)
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use("/static", express.static(path.join(__dirname, "/public")))
 
 app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
   res.status(500).send(`Something broke! ${err.stack}`)
 })
 
-app.get("/api/productos/listar", (req: Request, res: Response) => {
-  const result = products.getProducts()
-  result ? res.send(result) : res.send({ error: "No hay productos cargados." })
-})
-
-app.get("/api/productos/listar/:id", (req: Request, res: Response) => {
-  const id = parseInt(req.params.id)
-  const result = products.getProductById(id)
-  result ? res.send(result) : res.send({ error: "Producto no encontrado." })
-})
-
-app.post("/api/productos/guardar", (req: Request, res: Response) => {
-  const newProduct = req.body
-  const result = products.addProduct(newProduct)
-  res.send(result)
-})
+app.use("/api/productos", productos)
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}!`)
